171937b5e15800dec59da8f5994e341f2d3ac2ae
DHRUVPATEL8140 <dhruvpatel8251661.com>
2025-09-06 14:47:39 +0530

    first commit

--- build_files.sh
+++ build_files.sh
@@ -1,7 +1,7 @@
 echo "Building the project..."
 python3.13 -m pip install -r requirements.txt
 echo "Make Migration..."
-python3.13 manage.py makemigrations --noinput
-python3.13 manage.py migrate --noinput
+python3.13 manage.py makemigrations
+python3.13 manage.py migrate
 echo "Collect Static..."
-python3.13 manage.py collectstatic --noinput --clear
+python3.13 manage.py collectstatic --noinput
