7ce84acc9cb6c33377f039286ad2b1c54c56b8fa
DHRUVPATEL8140 <dhruvpatel8251661.com>
2025-09-06 13:34:14 +0530

    first commit

--- infinite/settings.py
+++ infinite/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-6em9se)6$0*$eg-mwyrcl3gwz@b@zl9r0)r3!86@_cu8p&rsl+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-      "rest_framework",
+    "rest_framework",
     "corsheaders",
     "products",
 ]
